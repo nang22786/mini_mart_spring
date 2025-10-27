0c8bf28e19e18291417d8470c01949868a71a726
nang22786 <samnangyorn1@gmail.com>
2025-10-27 23:21:17 +0700

    fix login

--- src/main/java/com/minimart/api/controller/UserController.java
+++ src/main/java/com/minimart/api/controller/UserController.java
@@ -65,7 +65,7 @@ public class UserController {
         }
     }
 
-    @PostMapping("/login")
+    @PostMapping(value = { "/login", "/login/" })
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
         AuthResponse response = userService.login(request);
 
